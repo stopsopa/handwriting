1ef6d51b47bcbddfa635423680b2c2900d206c81
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2025-11-15 17:54:37 +0000

    update

--- server.js
+++ server.js
@@ -18,45 +18,6 @@ if (!/^\d+$/.test(PORT)) {
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
-app.post("/generate", (req, res) => {
-  const { text, limit } = req.body;
-
-  const lines = smartLineBreak(text, limit);
-  const generatedHtml = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Handwriting Practice</title>
-            <link rel="stylesheet" href="../style.css" />
-        </head>
-        <body>
-            ${lines.map((line) => `<div class="line">${line}</div>`).join("")}
-        </body>
-        </html>
-    `;
-  const now = new Date();
-  const fileName = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(
-    2,
-    "0"
-  )}-${String(now.getDate()).padStart(2, "0")}-${String(
-    now.getHours()
-  ).padStart(2, "0")}_${String(now.getMinutes()).padStart(2, "0")}.html`;
-
-  const dirPath = path.join(__dirname, "generated");
-
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-  }
-
-  const filePath = path.join(dirPath, fileName);
-
-  fs.writeFileSync(filePath, generatedHtml);
-
-  res.json({ url: `/generated/${fileName}` });
-});
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`WYSIWYG Editor running at http://localhost:${PORT}`);
